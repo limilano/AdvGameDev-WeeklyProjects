70e12d3a901a5213d5218bca8955d1867cca8162
limilano <limilano@davidson.edu>
2025-01-30 18:31:41 -0500

    timer, course, more shapes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,22 +1,60 @@
+// https://www.youtube.com/watch?v=HLz_k6DSQvU
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using System.Collections;
+using System;
 
 public class Timer : MonoBehaviour
 {
+    bool stopwatchActive = false;
     public float currentTime;
-    public int startMinutes;
     public TMP_Text currentTimeText;
+    [SerializeField] private Collider feetCollider;
+    [SerializeField] private Collider startCollider;
+    [SerializeField] private Collider stopCollider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentTime = startMinutes * 60;
+        currentTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTimeText.text = currentTime.ToString();
+        if(CheckStart()) StartStopwatch();
+        if(CheckStop()) StopStopwatch();
+
+        if (stopwatchActive)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+
+    public void StartStopwatch()
+    {
+        Start();
+        stopwatchActive = true;
+    }
+
+    public void StopStopwatch()
+    {
+        stopwatchActive = false;
+    }
+
+    public bool CheckStart()
+    {
+        if(feetCollider.bounds.Intersects(startCollider.bounds)) return true;
+        return false;
+    }
+
+    public bool CheckStop()
+    {
+        if(feetCollider.bounds.Intersects(stopCollider.bounds)) return true;
+        return false;
     }
 }
