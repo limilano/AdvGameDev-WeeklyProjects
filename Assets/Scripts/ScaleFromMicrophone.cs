eca7a35832edf2cae4422203298c05fbadab8104
limilano <limilano@davidson.edu>
2025-02-20 19:38:40 -0500

    microphone system working

---
+++ Assets/Scripts/ScaleFromMicrophone.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ScaleFromMicrophone : MonoBehaviour
+{
+
+    public AudioSource source;
+    public Vector3 minScale;
+    public Vector3 maxScale;
+    public AudioLoudnessDetection detector;
+
+    public float loudnessSensitivity = 100;
+    public float threshold = 0.1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensitivity;
+        if (loudness < threshold) loudness = 0;
+        transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
+    }
+}
